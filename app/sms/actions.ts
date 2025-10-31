94f6f00badc1b0ff1ed975951798c64490cf1d47
awdrser <ddan990@naver.com>
2025-10-31 11:23:11 +0900

    sms 로그인 구현, prisma 세팅

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,3 +1,43 @@
 "use server";
 
-export async function smsVerification(prevState: any, formData: FormData) {}
+import { redirect } from "next/navigation";
+import validator from "validator";
+import z from "zod";
+
+interface ActionState {
+  token: boolean;
+  formErrors?: string[];
+}
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "잘못된 번호 형식입니다."
+  );
+const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      const flatten = z.flattenError(result.error);
+      console.log(flatten);
+      return { token: false, formErrors: flatten.formErrors };
+    } else {
+      return { token: true };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      const flatten = z.flattenError(result.error);
+      console.log(flatten);
+      return { token: true, formErrors: flatten.formErrors };
+    } else {
+      redirect("/");
+    }
+  }
+}
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -2,30 +2,42 @@
 import Button from "@/components/botton";
 import Input from "@/components/input";
 import { useActionState } from "react";
-import { smsVerification } from "./actions";
+import { smsLogin } from "./actions";
+
+const initialState = {
+  token: false,
+  formErrors: undefined,
+};
 
 export default function SMSLogin() {
-  const [state, dispatch] = useActionState(smsVerification, null);
+  const [state, dispatch] = useActionState(smsLogin, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">SMS Log in</h1>
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <Input
-          type="number"
-          placeholder="Phone number"
-          required
-          name="phoneNumber"
-        />
-        <Input
-          type="number"
-          placeholder="Verification code"
-          required
-          name="verifiCode"
-        />
-        <Button text="Verify" />
+      <form action={dispatch} className="flex flex-col gap-3">
+        {state?.token ? (
+          <Input
+            type="text"
+            placeholder="Verification code"
+            required
+            name="token"
+            min={100000}
+            max={999999}
+            errors={state.formErrors}
+          />
+        ) : (
+          <Input
+            type="number"
+            placeholder="Phone number"
+            required
+            name="phone"
+            errors={state.formErrors}
+          />
+        )}
+        <Button text={state.token ? "인증번호 확인" : "인증번호 받기"} />
       </form>
     </div>
   );
