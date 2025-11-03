0f7af640ff749aaaa06c327dce090b5ccfda1c85
awdrser <ddan990@naver.com>
2025-11-04 00:24:42 +0900

    8-3

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,10 @@
 "use server";
+import bcrypt from "bcrypt";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
 import { z } from "zod";
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from "../lib/constants";
+import db from "../lib/db";
 
 const crossCheckPassword = ({
   password,
@@ -14,13 +18,47 @@ const crossCheckPassword = ({
   } else return false;
 };
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
-    userName: z.string().min(2, "너무 짧습니다.").max(10, "너무 깁니다."),
-    email: z.email().toLowerCase().trim().toLowerCase(),
+    userName: z
+      .string()
+      .min(2, "너무 짧습니다.")
+      .max(10, "너무 깁니다.")
+      .refine(checkUniqueUsername, "이미 존재하는 이름입니다."),
+    email: z
+      .email()
+      .toLowerCase()
+      .trim()
+      .toLowerCase()
+      .refine(checkUniqueEmail, "이미 존재하는 이메일입니다."),
     password: z
       .string()
-      .min(10)
+      .min(10, "10자 이상이어야 합니다.")
       .regex(PASSWORD_REGEX, { error: "특수기호가 포함되어야 합니다." }),
     confirmPassword: z.string().min(PASSWORD_MIN_LENGTH),
   })
@@ -36,9 +74,28 @@ export async function createAccount(prevItem: any, formData: FormData) {
     password: formData.get("password"),
     confirmPassword: formData.get("confirmPassword"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     const flatten = z.flattenError(result.error);
     return { fieldErrors: flatten.fieldErrors };
+  } else {
+    const HashPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.userName,
+        email: result.data.email,
+        password: HashPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    const cookie = await getIronSession(await cookies(), {
+      cookieName: "carrot-market",
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    //@ts-ignore
+    cookie.id = user.id;
+    await cookie.save();
   }
 }
