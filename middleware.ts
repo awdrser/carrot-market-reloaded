b0f5532c945f6e4d834a5d88f8e13bf5f5e7809a
awdrser <ddan990@naver.com>
2025-11-05 23:34:28 +0900

    8-12

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,29 @@
 import { NextRequest } from "next/server";
+import getSession from "./app/lib/session";
+
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
 
 export async function middleware(request: NextRequest) {
-  console.log("hello");
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    if (!exists) {
+      Response.redirect(new URL("/", request.url));
+    }
+  } else {
+    if (!exists) {
+      Response.redirect(new URL("/profile", request.url));
+    }
+  }
 }
 
 export const config = {
